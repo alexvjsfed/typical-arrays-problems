507a2b55543d4df7fcc977384ce32acfd271d732
alexv <alexvjsfed@gmail.com>
2020-10-03 21:47:05 +0400

    Update index.js
    Implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  var result = 0;
+  if (Array.isArray(array)) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] < result)
+        result = array[i];
+    }
+  }
+  return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  var result = 0;
+  if (Array.isArray(array)) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > result) 
+        result = array[i];
+    }
+  }
+  return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  var result = 0;
+  if (Array.isArray(array) && array.length > 0) {
+    for (var i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+    result /= array.length;
+  }
+  return result;
 }
